56bf4f8737ed37a34c4bfa6cdeb66ce612e272a2
Al-Zahir <alzahir97@gmail.com>
2016-07-24 02:43:36 -0500

    Archery init

---
+++ Assets/Scripts/Other/FirstPersonCameraControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonCameraControl : MonoBehaviour {
+
+	public float firstPersonRotation;
+	public GameObject player;
+	public float lookSpeed = 1;
+
+	void Start(){
+
+		firstPersonRotation = 0;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//set firstPersonRotation back to 0 when transitioning, do later
+		float h = lookSpeed * Input.GetAxis("Mouse X");
+		float v = lookSpeed * Input.GetAxis("Mouse Y");
+		float clampNeg = -40;
+		float clampPos = 40;
+		firstPersonRotation = Mathf.Clamp(firstPersonRotation - v, clampNeg,
+			clampPos);
+		// Set player rotation to camera rotation :)
+		player.transform.Rotate(0,h,0);
+		transform.rotation = player.transform.rotation * Quaternion.AngleAxis(firstPersonRotation, Vector3.right);
+	}
+}

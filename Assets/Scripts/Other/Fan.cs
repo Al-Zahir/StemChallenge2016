2b2d6f058e56ce4a3e5469309efb83b842655e61
Quadmium <quadmium@gmail.com>
2016-08-12 17:24:59 -0500

    merge, final temple lights and script part 1

---
+++ Assets/Scripts/Other/Fan.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fan : MonoBehaviour {
+
+    public float angle = 20;
+
+	void FixedUpdate () {
+        transform.Rotate(transform.up * angle * Time.deltaTime);
+	}
+}
